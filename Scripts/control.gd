09841bea58969c15de50259e4db605da282ef87f
calvinrsun <notrandomhumon@gmail.com>
2026-09-20 13:28:06 -0700

    main menu is cooler now

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,8 +1,10 @@
 extends Control
 # Called when the node enters the scene tree for the first time.
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/tag_map_1.tscn")
 
 func _on_dont_play_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tag_map_1.tscn")
