c1f9ffe8c744ba251e61f62812a2177db163c0a1
calvinrsun <notrandomhumon@gmail.com>
2026-09-20 21:37:27 -0700

    minor things

--- Scripts/tag_map_1.gd
+++ Scripts/tag_map_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 #spawns stuff
 @onready var red_character: CharacterBody2D = %red_character
 @onready var blue_character: CharacterBody2D = %blue_character
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 var possible_coords = [Vector2(-94, -474),Vector2(397, -601),Vector2(405, -384),Vector2(868, -574),Vector2(1034, -462)]
 
@@ -9,11 +10,12 @@ var time_until_spawn = 3.0
 
 @onready var scene = load("res://scenes/Double jump.tscn")
 func spawn_double_jump(coords):
-	var scene_to_spawn = scene.instantiate()
-	scene_to_spawn.player_1_ref = blue_character
-	scene_to_spawn.player_2_ref = red_character
-	add_child(scene_to_spawn)
-	scene_to_spawn.set_global_position(coords)
+	if canvas_layer.still_running:
+		var scene_to_spawn = scene.instantiate()
+		scene_to_spawn.player_1_ref = blue_character
+		scene_to_spawn.player_2_ref = red_character
+		add_child(scene_to_spawn)
+		scene_to_spawn.set_global_position(coords)
 func _process(delta):
 	print(time_until_spawn)
 	time_until_spawn -= delta
